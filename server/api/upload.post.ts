0ddcc78f253d2ca606ddc913b5b74528c0f90108
bhaaratkrishnan <bhaaratkrishnan29113@gmail.com>
2023-11-04 22:24:17 +0530

    first commit

---
+++ server/api/upload.post.ts
@@ -0,0 +1,21 @@
+import { Storage } from "@google-cloud/storage";
+
+export default defineEventHandler(async (event) => {
+  const formData = await readFormData(event);
+  if (formData.get("upload") === null) {
+    return;
+  }
+  const file = formData.get("upload") as File;
+  const fileBuffer = Buffer.from(await file.arrayBuffer());
+  const bucketName = "test-bucket-code-vipa";
+  const storage = new Storage();
+  const bucket = storage.bucket(bucketName);
+  const fileUpload = bucket.file(file.name);
+  await fileUpload.save(fileBuffer);
+  const [url] = await storage.bucket(bucketName).file(file.name).getSignedUrl({
+    expires:Date.now() + 15 * 60 *1000,
+    action:"read",
+    version:"v2"
+  });
+  return fileUpload.makePublic();
+});
